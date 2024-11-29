da50dfd9e91964f96d0d05089f9bc24e63fc8817
JavaScripHTML <josuequilichelopez@gmail.com>
2024-11-28 23:46:14 -0500

    Limpieza2.0

--- app/src/main/java/com/example/lacteosdisanaapp/TicketActivity.java
+++ app/src/main/java/com/example/lacteosdisanaapp/TicketActivity.java
@@ -1,7 +1,5 @@
 package com.example.lacteosdisanaapp;
 
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,7 +8,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.lacteosdisanaapp.Modelo.Pedido;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class TicketActivity extends AppCompatActivity {
